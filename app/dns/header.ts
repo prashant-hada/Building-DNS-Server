896ab6658d0452a3676d02c7a0a7fbaf078fc023
Prashant <hadaprashant2002@gmail.com>
2024-09-20 20:19:47 +0530

    stage-Write Header

---
+++ app/dns/header.ts
@@ -0,0 +1,50 @@
+export enum OpCode{
+    STANDARD_QUERY = 0,
+    INVERSE_QUERY = 1
+}
+
+export enum ResponseCode{
+    NO_ERROR = 0,
+    FORMAT_ERROR = 1,
+    SERVER_ERROR = 2,
+    NAME_ERROR = 3
+}
+export interface TDNSHeader{
+    id: number,
+    qr: number,
+    opCode:OpCode,
+    aa: number,
+    tc: number,
+    rd: number,
+    ra :number,
+    z: number,
+    rCode : ResponseCode
+    qdCount : number,
+    anCount : number,
+    nsCount: number,
+    arCount : number
+}
+
+export class DNSHeader{
+    static write(value:TDNSHeader) : Buffer {
+        const header = Buffer.alloc(12);
+        const flags =
+          value.qr |
+          value.opCode |
+          value.aa |
+          value.tc |
+          value.rd |
+          value.ra |
+          value.z |
+          value.rCode;
+
+          header.writeInt16BE(value.id , 0);
+          header.writeInt16BE(flags , 2);
+          header.writeInt16BE(value.qdCount , 4);
+          header.writeInt16BE(value.anCount , 6);
+          header.writeInt16BE(value.nsCount , 8);
+          header.writeInt16BE(value.arCount , 10);
+
+        return header
+    }
+}
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,21 @@
 import * as dgram from "dgram";
+import { TDNSHeader , DNSHeader, OpCode, ResponseCode } from "./dns/header";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const defaultHeader : TDNSHeader = {
+    id: 1234,
+    qr: 1,
+    opCode: OpCode.STANDARD_QUERY,
+    aa:  0,
+    tc: 0,
+    rd: 0,
+    ra: 0,
+    z: 0,
+    rCode : ResponseCode.NO_ERROR,
+    qdCount : 0,
+    anCount : 0,
+    nsCount : 0,
+    arCount : 0
+} 
 
 // Uncomment this block to pass the first stage
 //
@@ -11,7 +25,8 @@ udpSocket.bind(2053, "127.0.0.1");
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
+        const header = DNSHeader.write(defaultHeader);
+        const response = Buffer.concat([header]);
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
