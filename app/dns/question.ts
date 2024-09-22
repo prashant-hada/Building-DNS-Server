93ecbacd5a5b91ae165746cd7105d094a93985a2
Prashant <hadaprashant2002@gmail.com>
2024-09-22 23:48:31 +0530

    Stage - Write question completed

--- app/dns/question.ts
+++ app/dns/question.ts
@@ -1,5 +1,34 @@
-export class Question{
-    constructor(){
+export enum DNSQuestionType{
+    A = 1,
+    NS = 2,
+    MD = 3,
+    MF = 4,
+    CNAME = 5
+}
+export enum DNSClassCodes{
+    IN = 1,
+    CS = 2,
+}
 
-    }
+export interface _DNSQuestion{
+   name : string,
+   type : DNSQuestionType,
+   classCode : DNSClassCodes 
+}
+export class Question {
+  static write(questions:_DNSQuestion[]) {
+    return Buffer.concat(
+      questions.map((question) => {
+        const { name, type, classCode } = question;
+
+        const str =name.split('.').map(n=>`${String.fromCharCode(n.length)}${n}`).join("");
+        const typeAndClass = Buffer.alloc(4);
+
+        typeAndClass.writeInt16BE(type);
+        typeAndClass.writeInt16BE(classCode, 2);
+
+        return Buffer.concat([Buffer.from(str + "\0", "binary"), typeAndClass]);
+      })
+    );
+  }
 }
