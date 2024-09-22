1a4c92a2368cbb71b45d542f96063365805c408c
Prashant <hadaprashant2002@gmail.com>
2024-09-22 23:48:31 +0530

    Stage - Write question completed

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,10 @@
 import * as dgram from "dgram";
 import { TDNSHeader , DNSHeader, OpCode, ResponseCode } from "./dns/header";
-import { Question } from "./dns/question";
+import { Question , _DNSQuestion, DNSClassCodes, DNSQuestionType} from "./dns/question";
 
 const defaultHeader : TDNSHeader = {
     id: 1234,
-    qr: 1,
+    qr: 1 << 15,
     opCode: OpCode.STANDARD_QUERY,
     aa:  0,
     tc: 0,
@@ -18,6 +18,13 @@ const defaultHeader : TDNSHeader = {
     arCount : 0
 } 
 
+const defaultQuestion : _DNSQuestion ={
+    name : "codecrafters.io",
+    classCode : DNSClassCodes.IN,
+    type : DNSQuestionType.A
+}
+
+
 // Uncomment this block to pass the first stage
 //
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
@@ -27,7 +34,8 @@ udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
         const header = DNSHeader.write(defaultHeader);
-        const response = Buffer.concat([header]);
+        const question = Question.write([defaultQuestion])
+        const response = Buffer.concat([header, question]);
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
